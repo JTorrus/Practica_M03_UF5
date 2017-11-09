69f42d91b4a4dd74a3ec8e58da6a7f9dbec8817c
JTorrus <jtorrusgalindo@gmail.com>
2017-11-09 16:39:14 +0100

    Modifying class Answer

--- src/classes/Answer.java
+++ src/classes/Answer.java
@@ -3,7 +3,6 @@ package classes;
 public class Answer {
     private String text;
     private boolean correctAnswer;
-    private int Javi;
 
     public Answer(String text, boolean correctAnswer) {
         this.text = text;

88d43b514623a9abc196ec308f21e667c956feb6
Albert Ibanez <ait98@hotmail.com>
2017-11-09 16:37:53 +0100

    Test Commit

--- src/classes/Answer.java
+++ src/classes/Answer.java
@@ -3,6 +3,7 @@ package classes;
 public class Answer {
     private String text;
     private boolean correctAnswer;
+    private int Javi;
 
     public Answer(String text, boolean correctAnswer) {
         this.text = text;

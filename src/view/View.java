d4b3a8add346c3ec0876e8688d6e92cd01814e2e
JTorrus <jtorrusgalindo@gmail.com>
2017-11-09 17:33:36 +0100

    Creation of buttons in View. WIP.

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,36 @@
 package view;
 
-public class View {
+import javax.swing.*;
+import java.awt.*;
+
+public class View extends JFrame {
+    private JButton[] cells;
+
+    public View() {
+        this.setTitle("JeoPardy");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        addComponentsToPane(this.getContentPane());
+        this.pack();
+        this.setVisible(true);
+    }
+
+    void addComponentsToPane(Container pane) {
+        pane.setLayout(new BorderLayout(8, 8));
+
+        JPanel cellsPanel = new JPanel(new GridLayout(6, 6));
+        final int GRID_SIZE = 36;
+
+        cells = new JButton[GRID_SIZE];
+
+        for (int i = 0; i < GRID_SIZE; i++) {
+            cells[i] = new JButton();
+            cells[i].setPreferredSize(new Dimension(120,90));
+
+            cellsPanel.add(cells[i]);
+        }
+
+        pane.add(cellsPanel);
+
+    }
 }

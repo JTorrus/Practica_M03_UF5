22e65355d618b0f4d0fe99f89f3fe1be97de4a60
JTorrus <jtorrusgalindo@gmail.com>
2017-11-16 17:51:39 +0100

    Added ToString in Question clas. Deleted ProgramRun. Added categories.txt.

--- src/model/Question.java
+++ src/model/Question.java
@@ -3,13 +3,21 @@ package model;
 import java.util.ArrayList;
 
 public class Question {
-    private String name;
+    private String text;
     private int pts;
     private ArrayList<Answer> answers;
 
-    public Question(String name, int pts, ArrayList<Answer> answers) {
-        this.name = name;
+    public Question(String text, int pts, ArrayList <Answer> answers) {
+        this.text = text;
         this.pts = pts;
         this.answers = answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "text='" + text + '\'' +
+                ", pts=" + pts +
+                '}';
     }
 }

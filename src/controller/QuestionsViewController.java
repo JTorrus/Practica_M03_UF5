5ca05f62ab20d5a277279197a79156af0439ecd2
Albert Ibanez <ait98@hotmail.com>
2017-11-28 16:33:41 +0100

    Load data to view

--- src/controller/QuestionsViewController.java
+++ src/controller/QuestionsViewController.java
@@ -7,11 +7,20 @@ import view.QuestionsView;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 
-public class QuestionsViewController implements ActionListener{
-    public QuestionsView view;
-    public Player p1;
-    public Player p2;
+public class QuestionsViewController implements ActionListener {
+    private QuestionsView view;
+    private Player p1;
+    private Player p2;
+    private HashMap m1;
+    private HashMap m2;
 
     public QuestionsViewController(Player p1, Player p2) {
         this.p1 = p1;
@@ -20,14 +29,39 @@ public class QuestionsViewController implements ActionListener{
         for (int i = 0; i < view.cells.length; i++) {
             this.view.cells[i].addActionListener(this);
         }
+        loadData();
     }
 
-   @Override
-   public void actionPerformed(ActionEvent e) {
-       for (int i = 0; i < view.cells.length; i++) {
-           if (view.cells[i] == e.getSource()) {
-               view.dispose();
-           }
-       }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < view.cells.length; i++) {
+            if (view.cells[i] == e.getSource()) {
+                view.dispose();
+            }
+        }
+    }
+
+    private void loadData() {
+        BufferedReader br = null;
+        String[] datos = null;
+        HashMap m1 = new HashMap();
+        HashMap m2 = new HashMap();
+        Path p1 = FileSystems.getDefault().getPath("./categories.txt");
+        try {
+            br = Files.newBufferedReader(p1, StandardCharsets.UTF_8);
+            String linea;
+            linea = br.readLine();
+            datos = linea.split(";");
+            for (int i = 0; i <= 6; i++){
+                view.cells[i].setText(datos[i]);
+            }
+            while ((linea = br.readLine()) != null) {
+                datos = linea.split(";");
+
+            }
+        } catch (IOException e) {
+            System.err.println("Error al acceder la fichero");
+        }
+
     }
 }

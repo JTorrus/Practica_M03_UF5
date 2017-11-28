73fd50023050699514f942ddfbcba8c43bc46411
JTorrus <jtorrusgalindo@gmail.com>
2017-11-28 16:31:26 +0100

    Collections in Category and Question changed to ArrayList.

--- src/model/Category.java
+++ src/model/Category.java
@@ -1,13 +1,14 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Category {
     private String name;
-    private HashMap<Integer, HashMap> questions;
+    ArrayList<Question> questions;
 
     public Category(String name) {
         this.name = name;
-        this.questions = new HashMap<>();
+        this.questions = new ArrayList<>();
     }
 }
